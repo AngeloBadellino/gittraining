5b36fb6e3a8dad25c7c72b1eac6a333612e5a87c
Angelo Badellino <badellinoa@gmail.com>
2019-01-17 16:03:13 +0100

    Commit to branch Angelo

---
+++ GitTraining.GetWordCount/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace GitTraining.GetWordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long count = GetWordCount(args[0]);
+            Console.WriteLine(String.Format("There are {0} words", count));
+        }
+
+        static long GetWordCount(string text) {
+            return text.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
